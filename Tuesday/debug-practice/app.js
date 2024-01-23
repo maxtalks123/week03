cce7a3e2da3febcb8b2c4f004c96169f4c9c6e5f
Max Emerson <maxemerson2013@gmail.com>
2024-01-23 11:45:46 +0000

    updated with next exercise

---
+++ Tuesday/debug-practice/app.js
@@ -0,0 +1,8 @@
+console.log("hello,world");
+const myVariable = "yada, yada, yada";
+debugger;
+
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+//in inspect sources I added a conditional event breakpoint where code would stop at if i < 50. the debugger code here also stopped code from running until I skipped over it.
