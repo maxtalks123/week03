967c5052a0e4ee00b27f7c55f639ac20ae1e589b
Max Emerson <maxemerson2013@gmail.com>
2024-01-23 10:52:17 +0000

    workshops from today

---
+++ Tuesday/API-practice/app.js
@@ -0,0 +1,5 @@
+//find api that is interesting to me and make a request and display on the screen
+
+const data = [];
+const natDisaster = document.getElementById("natDisaster");
+https://api.reliefweb.int/v1/reports?appname=apidoc&limit=2
